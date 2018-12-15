77ab49390a158cf7fcef05d3518443f5b3b25721
novicailievski93 <novica.ilievski@intertec.io>
2018-12-15 18:24:38 +0100

    ionic-demo: Add alert and toast controllers for better experience

--- src/pages/drink/drink.ts
+++ src/pages/drink/drink.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {IonicPage} from 'ionic-angular';
+import {AlertController, IonicPage, ToastController, ToastOptions} from 'ionic-angular';
 import {ApiService} from "../../providers/api.service";
 import {MenuItemModel} from "../../models/menu-item.model";
 
@@ -14,7 +14,9 @@ export class DrinkPage implements OnInit {
 
   public drinks: Array<MenuItemModel>;
 
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController) {
   }
 
   public ngOnInit(): void {
@@ -23,6 +25,33 @@ export class DrinkPage implements OnInit {
     }, (err) => {
       console.log(err);
     })
+  }
+
+  public showConfirm(): void {
+    const confirm = this.alertCtrl.create({
+      title: 'The delivery is fast and free!',
+      message: 'First you need to register on our platform, and after that you will be able to order stuff from our restaurant.',
+      buttons: [
+        {
+          text: 'No, thanks',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Register',
+          handler: () => {
+            const toastOpts: ToastOptions = {
+              message: 'Now we should be redirected to the registration screen, but there is no registration screen :)',
+              duration: 5000,
+              position: 'bottom'
+            };
+            let registerToast = this.toastCtrl.create(toastOpts);
+            registerToast.present();
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
 

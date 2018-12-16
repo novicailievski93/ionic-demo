092cde81ecb79cb3da0f749d51251dd2aa99555f
novicailievski93 <novica.ilievski@intertec.io>
2018-12-16 10:35:41 +0100

    ionic-demo: Generated a component for listing the menu items

--- src/pages/drink/drink.ts
+++ src/pages/drink/drink.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {AlertController, IonicPage, ToastController, ToastOptions} from 'ionic-angular';
+import {IonicPage} from 'ionic-angular';
 import {ApiService} from "../../providers/api.service";
 import {MenuItemModel} from "../../models/menu-item.model";
 
 @IonicPage()
-
 @Component({
   selector: 'page-drink',
   templateUrl: 'drink.html',
@@ -14,9 +13,7 @@ export class DrinkPage implements OnInit {
 
   public drinks: Array<MenuItemModel>;
 
-  constructor(private apiService: ApiService,
-              private alertCtrl: AlertController,
-              private toastCtrl: ToastController) {
+  constructor(private apiService: ApiService) {
   }
 
   public ngOnInit(): void {
@@ -25,33 +22,6 @@ export class DrinkPage implements OnInit {
     }, (err) => {
       console.log(err);
     })
-  }
-
-  public showConfirm(): void {
-    const confirm = this.alertCtrl.create({
-      title: 'The delivery is fast and free!',
-      message: 'First you need to register on our platform, and after that you will be able to order stuff from our restaurant.',
-      buttons: [
-        {
-          text: 'No, thanks',
-          handler: () => {
-          }
-        },
-        {
-          text: 'Register',
-          handler: () => {
-            const toastOpts: ToastOptions = {
-              message: 'Now we should be redirected to the registration screen, but there is no registration screen :)',
-              duration: 5000,
-              position: 'bottom'
-            };
-            let registerToast = this.toastCtrl.create(toastOpts);
-            registerToast.present();
-          }
-        }
-      ]
-    });
-    confirm.present();
   }
 }
 
--- src/pages/food/food.ts
+++ src/pages/food/food.ts
@@ -1,55 +1,27 @@
 import {Component, OnInit} from '@angular/core';
-import {AlertController, IonicPage, ToastController, ToastOptions} from 'ionic-angular';
+import {IonicPage} from 'ionic-angular';
 import {ApiService} from "../../providers/api.service";
 import {MenuItemModel} from "../../models/menu-item.model";
 
 @IonicPage()
-
 @Component({
   selector: 'page-food',
   templateUrl: 'food.html',
 })
+
 export class FoodPage implements OnInit {
 
   public food: Array<MenuItemModel>;
 
-  constructor(private apiService: ApiService,
-              private alertCtrl: AlertController,
-              private toastCtrl: ToastController) {
+  constructor(private apiService: ApiService) {
   }
 
   public ngOnInit(): void {
+
     this.apiService.getAllFood().subscribe((res) => {
       this.food = res;
     }, (err) => {
       console.log(err);
     });
-  }
-
-  public showConfirm(): void {
-    const confirm = this.alertCtrl.create({
-      title: 'The delivery is fast and free!',
-      message: 'First you need to register on our platform, and after that you will be able to order stuff from our restaurant.',
-      buttons: [
-        {
-          text: 'No, thanks',
-          handler: () => {
-          }
-        },
-        {
-          text: 'Register',
-          handler: () => {
-            const toastOpts: ToastOptions = {
-              message: 'Now we should be redirected to the registration screen, but there is no registration screen :)',
-              duration: 5000,
-              position: 'bottom'
-            };
-            let registerToast = this.toastCtrl.create(toastOpts);
-            registerToast.present();
-          }
-        }
-      ]
-    });
-    confirm.present();
   }
 }
